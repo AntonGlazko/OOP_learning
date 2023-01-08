7b63f3ff328a1e5479b87554721c9e98daa60a25
anton-glazko <antonforpython@gmail.com>
2023-01-08 05:48:06 +0300

    __init__, __del__, __new__, staticmethod, classmethod

--- OOP_python_concept.py
+++ OOP_python_concept.py
@@ -24,3 +24,29 @@ print(f"The Dog's namespace contains: {Dog.breed}") # and the class namespace ha
 setattr(Dog, 'size', 'big')
 print(Dog.size)
 
+class SomeClass:
+    PROPERTY_1 = 0    # this args related with class
+    PROPERTY_2 = 100
+    # this method refers to the class directly
+    @classmethod
+    def check_valid(cls, arg):
+        return cls.PROPERTY_1 <= arg <= cls.PROPERTY_2
+    # (self) is a reference to an existing instance of the class that we created earlier
+    # class invokes method __new__() before instance creating
+    def __new__(cls, *args, **kwargs):
+        print(f'The __new__ method is called')
+        return super().__new__(cls)
+    # When a class defines an __init__() method, class instantiation automatically
+    # invokes __init__() for the newly created class instance
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        pass
+    # __del__ is called when an object is garbage collected
+    # which happens at some point after all references to the object have been deleted
+    def __del__(self):
+        pass
+    # this method doesn't require references to the instance or class
+    @staticmethod
+    def do_something_staticly(x, y):
+        return (x**x + y**y)
